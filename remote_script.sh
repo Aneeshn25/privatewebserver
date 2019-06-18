54d3fd0d18fb6deab7129e31b8fd920c125685fa
Aneesh Nelavelly <35740650+Aneeshn25@users.noreply.github.com>
2019-06-18 17:12:04 -0400

    Update remote_script.sh

--- remote_script.sh
+++ remote_script.sh
@@ -1,7 +1,7 @@
 sudo su
 cd /var/www/html
 rm -Rf html
-git clone https://github.com/Aneeshn25/html.git
-mv html/index.html ./index.html
+git clone https://github.com/Aneeshn25/privatewebserver.git
+mv privatewebserver/index.html ./index.html
 rm -Rf html
 exit
